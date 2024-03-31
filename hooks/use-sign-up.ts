d34c015e0212257a11fe947f77fd3ea48b27244f
CasianLW <ciorbacasian@gmail.com>
2024-03-31 17:45:34 +0200

    sign up / register hook for its component

---
+++ hooks/use-sign-up.ts
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Auth } from "aws-amplify";
+import { useNavigation } from "@react-navigation/native";
+import { AuthStackParamList } from "@/app/_layout";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useAuth } from "@/context/auth-context";
+
+type SignUpNavigationProp = StackNavigationProp<AuthStackParamList, "SignUp">;
+
+const useSignUp = (setIsAuthenticated: (value: boolean) => void) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isConfirmModalVisible, setIsConfirmModalVisible] = useState(false);
+  const [confirmationCode, setConfirmationCode] = useState("");
+  const { setUser } = useAuth();
+
+  const handleSignUp = async () => {
+    try {
+      await Auth.signUp({ username, password });
+      setIsConfirmModalVisible(true);
+    } catch (error) {
+      console.error("Sign up error", error);
+    }
+  };
+
+  const handleConfirmSignUp = async () => {
+    try {
+      await Auth.confirmSignUp(username, confirmationCode);
+      const user = await Auth.signIn(username, password);
+      setUser(user);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Error confirming sign up:", error);
+    } finally {
+      setIsConfirmModalVisible(false);
+    }
+  };
+
+  return {
+    username,
+    setUsername,
+    password,
+    setPassword,
+    isConfirmModalVisible,
+    setIsConfirmModalVisible,
+    confirmationCode,
+    setConfirmationCode,
+    handleSignUp,
+    handleConfirmSignUp,
+  };
+};
+
+export default useSignUp;
