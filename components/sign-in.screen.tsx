a58c702e69d4623ff37e5fab200233272c20af82
CasianLW <ciorbacasian@gmail.com>
2024-03-31 16:16:39 +0200

    added design to screen

--- components/sign-in.screen.tsx
+++ components/sign-in.screen.tsx
@@ -1,10 +1,22 @@
 import React, { FC, useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
 import { Amplify, Auth } from "aws-amplify";
 import { useAuth } from "@/context/auth-context";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthStackParamList } from "@/app/_layout";
+
+import globalStyles from "@/styles/globalStyles";
+import { LinearGradient } from "expo-linear-gradient";
+import ButtonComponent from "./general/button-component";
 
 type SignInNavigationProp = StackNavigationProp<AuthStackParamList, "SignIn">;
 
@@ -36,33 +48,116 @@ const SignInScreen: FC<SigninScreenProps> = ({ setIsAuthenticated }) => {
   };
 
   return (
-    <View className={"!bg-red-600"} style={styles.container}>
-      <Text>Sign In</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        placeholderTextColor="#888"
-        onChangeText={(text) => setUsername(text)}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="#888"
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Sign In" onPress={handleSignIn} disabled={isSigningIn} />
+    <View className={"bg-app-black-200 "} style={styles.container}>
+      <View className="bg-app-white-100 w-full">
+        <Image
+          source={require("@/assets/images/login-image.png")}
+          className="bg-cover w-full "
+        />
+      </View>
+      <View className="bg-app-white-100 w-11/12 mt-auto mx-10 rounded-3xl">
+        <Text
+          className="text-app-black-300 my-6 text-center"
+          style={globalStyles.title}
+        >
+          Sign In
+        </Text>
+        <TextInput
+          className="mx-auto"
+          style={globalStyles.lightInput}
+          placeholder="Username"
+          placeholderTextColor="#888"
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <TextInput
+          className="mx-auto"
+          style={globalStyles.lightInput}
+          placeholder="Password"
+          placeholderTextColor="#888"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry
+        />
+        <View className="w-1/2 m-auto">
+          <ButtonComponent
+            onPress={handleSignIn}
+            title="Log in"
+            disabled={isSigningIn}
+            secondary={false}
+            white={true}
+          />
+        </View>
+        {/* <Button title="Sign In" onPress={handleSignIn} disabled={isSigningIn} /> */}
 
-      <Button
-        title="Don't have an account? Sign Up"
-        onPress={() => navigation.navigate("SignUp")}
-      />
-      <Button
-        title="Forgot Password?"
-        onPress={() => navigation.navigate("ForgotPassword")}
-      />
+        <Button
+          color={"#03050C"}
+          title="Forgot Password?"
+          onPress={() => navigation.navigate("ForgotPassword")}
+        />
+      </View>
+      {/* <View className="mb-44 ">
+        <LinearGradient
+          colors={["#9A6B36", "#CCA373", "#9A6B36"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={globalStyles.gradient}
+        >
+          <Button
+            color={"#03050C"}
+            title="Don't have an account? Sign Up"
+            onPress={() => navigation.navigate("SignUp")}
+          />
+        </LinearGradient>
+      </View> */}
+      <View className="mb-44 mt-4 ">
+        <ButtonComponent
+          onPress={() => navigation.navigate("SignUp")}
+          secondary={true}
+          title="Don't have an account? Sign Up"
+        />
+        {/* <Pressable
+          onPress={() => navigation.navigate("SignUp")}
+          style={({ pressed }) => [
+            {
+              // backgroundColor: pressed ? '#DDDDDD' : 'transparent',
+              opacity: pressed ? 0.8 : 1,
+            }, // Your existing styles
+          ]}
+        >
+          {({ pressed }) => (
+            <LinearGradient
+              colors={["#CB9684", "#895442", "#CB9684"]}
+              start={{ x: 0, y: 0.5 }}
+              end={{ x: 1, y: 0.5 }}
+              style={{ borderRadius: 10, overflow: "hidden", padding: 1 }}
+            >
+              <LinearGradient
+                colors={
+                  !pressed
+                    ? ["#895442", "#CB9684", "#895442"]
+                    : ["#03050C", "#03050C", "#03050C"]
+                }
+                // colors={["#895442", "#CB9684", "#895442"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={{
+                  padding: 15,
+                  borderRadius: 9,
+                  backgroundColor: "#03050C ",
+                }}
+              >
+                <Text
+                  className="font-semibold"
+                  style={{ textAlign: "center", color: "#fff" }}
+                >
+                  Don't have an account? Sign Up
+                </Text>
+              </LinearGradient>
+            </LinearGradient>
+          )}
+        </Pressable> */}
+      </View>
     </View>
   );
 };
