da2aebf7a5827d48a4c97e6ee8b0fb9b672e0449
CasianLW <ciorbacasian@gmail.com>
2024-03-31 16:15:34 +0200

    global styles totle and lightinput

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -12,4 +12,21 @@ export default StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
+  lightInput: {
+    width: "80%",
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: "#f5f5f5",
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "400",
+  },
+
+  gradient: {
+    borderRadius: 5, // If you want rounded corners
+    padding: 15, // Adjust the padding as needed
+    alignItems: "center",
+  },
 });
