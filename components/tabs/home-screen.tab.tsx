b52bccc44182cacbba6dbcff320a875983e6b4fd
CasianLW <ciorbacasian@gmail.com>
2024-05-14 09:10:17 +0200

    updated homepage

--- components/tabs/home-screen.tab.tsx
+++ components/tabs/home-screen.tab.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { FC, useState } from "react";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 import { Auth } from "aws-amplify";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -9,12 +9,23 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/stores/main-store";
 import { useDispatch } from "react-redux";
 import { clearUser } from "@/stores/slices/auth-slice";
+import { useQuery } from "@tanstack/react-query";
+import { fetchVehicles } from "@/helpers/vehicle-query-list";
+import { VehicleRecord } from "@/interfaces/vehicle-interface";
+import SelectMake from "../vehicle/make-select-component";
 
 type SignInNavigationProp = StackNavigationProp<AuthStackParamList, "SignIn">;
 type HomeScreenProps = {
   setIsAuthenticated: (value: boolean) => void;
 };
 const HomeScreen: FC<HomeScreenProps> = ({ setIsAuthenticated }) => {
+  // const [make, setMake] = useState("");
+  // const { data, error, isLoading } = useQuery({
+  //   queryKey: ["vehicles", make],
+  //   queryFn: () => fetchVehicles(make),
+  //   enabled: !!make, // This will ensure the query only runs when 'make' is not empty
+  // });
+
   // const { user, signOut } = useAuth(); // Use the useAuth hook to access user and signOut function
   const user = useSelector((state: RootState) => state.auth.userData);
   const dispatch = useDispatch();
@@ -42,8 +53,22 @@ const HomeScreen: FC<HomeScreenProps> = ({ setIsAuthenticated }) => {
         You are now signed in! {user?.email ?? ""}
         {/* {user?.signInUserSession?.idToken?.payload?.email ?? ""} */}
       </Text>
-      <Button title="LOG USER" onPress={() => logUser(user)} />
+      {/* <Button title="LOG USER" onPress={() => logUser(user)} /> */}
       <Button title="Sign Out" onPress={signOutAppp} />
+      {/* <View> */}
+      {/* <TextInput
+        style={styles.input}
+        placeholder="Enter car make"
+        placeholderTextColor={"gray"}
+        onChangeText={setMake}
+      />
+      {data?.records.map((vehicle: VehicleRecord) => (
+        <Text key={vehicle.recordid}>
+          {vehicle.fields.make} - {vehicle.fields.model} ({vehicle.fields.year})
+        </Text>
+      ))} */}
+      {/* </View> */}
+      <SelectMake />
       {/* Add your home screen content here */}
     </View>
   );
@@ -52,12 +77,21 @@ const HomeScreen: FC<HomeScreenProps> = ({ setIsAuthenticated }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+    marginTop: 50,
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    width: "70%",
+    marginHorizontal: "auto",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
