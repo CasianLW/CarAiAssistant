7a6ad12f2fb1e26a42c824155a4abef230f876dc
CasianLW <ciorbacasian@gmail.com>
2024-06-11 08:15:56 +0200

    mono->urbanist

--- components/styled-text.tsx
+++ components/styled-text.tsx
@@ -1,5 +1,8 @@
 import { Text, TextProps } from "./themed";
 
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
+export function UrbanistText(props: TextProps) {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: "UrbanistItalic" }]} />
+  );
+  // return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
