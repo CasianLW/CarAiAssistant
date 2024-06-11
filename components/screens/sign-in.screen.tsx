8d831dfe0a61ec7d476a11225270bf53e087c6ba
CasianLW <ciorbacasian@gmail.com>
2024-06-11 08:17:00 +0200

    added onboarding debugg (flse localstorage variable)

--- components/screens/sign-in.screen.tsx
+++ components/screens/sign-in.screen.tsx
@@ -1,24 +1,15 @@
-import React, { FC, useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  StyleSheet,
-  Image,
-  Pressable,
-} from "react-native";
-import { Amplify, Auth } from "aws-amplify";
-import { useAuth } from "@/context/auth-context";
+// src/components/screens/SignInScreen.tsx
+import React, { FC, useEffect, useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthStackParamList } from "@/app/_layout";
-
 import globalStyles from "@/styles/global.styles";
 import ButtonComponent from "../general/button-component";
 import useSignIn from "@/hooks/use-sign-in.hook";
 import { logAsGuest } from "@/stores/slices/auth-slice";
 import { useDispatch } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type SignInNavigationProp = StackNavigationProp<AuthStackParamList, "SignIn">;
 
@@ -34,22 +25,41 @@ const SignInScreen: FC = () => {
   const navigation = useNavigation<SignInNavigationProp>();
   const dispatch = useDispatch();
 
+  const handleResetOnboarding = async () => {
+    await AsyncStorage.setItem("hasOnboarded", "false");
+    navigation.replace("Onboarding");
+  };
+  const [hasOnboardedValue, setHasOnboardedValue] = useState<string | null>(
+    null
+  );
+
+  useEffect(() => {
+    const getHasOnboarded = async () => {
+      const value = await AsyncStorage.getItem("hasOnboarded");
+      setHasOnboardedValue(value);
+    };
+
+    getHasOnboarded();
+  }, []);
+
   return (
-    <View className={"bg-app-black-200 "} style={styles.container}>
-      <View className="bg-app-white-100 w-full ">
+    <View className="bg-app-black-200" style={styles.container}>
+      <View className="bg-app-white-100 w-full">
         {/* <Image
-          source={require("@/assets/images/app-ressources/login-image.webp")}
-          className="bg-cover w-full mt-44 "
+          source={require('@/assets/images/app-ressources/login-image.webp')}
+          className="bg-cover w-full mt-44"
         /> */}
       </View>
       <View className="absolute z-10 w-11/12 top-[180px]">
-        <View className="bg-app-white-100 mt-auto  rounded-3xl p-4">
+        <View className="bg-app-white-100 mt-auto rounded-3xl p-4">
           <Text
             className="text-app-black-300 mb-6 mt-2 text-center"
             style={globalStyles.title}
           >
             Sign In
           </Text>
+          <Text>Current Onboarding State: {hasOnboardedValue}</Text>
+
           <TextInput
             className="mx-auto"
             style={globalStyles.lightInput}
@@ -76,28 +86,13 @@ const SignInScreen: FC = () => {
               white={false}
             />
           </View>
-          {/* <Button title="Sign In" onPress={handleSignIn} disabled={isSigningIn} /> */}
           <Button
             color={"#03050C"}
             title="Forgot Password?"
             onPress={() => navigation.navigate("ForgotPassword")}
           />
         </View>
-        {/* <View className="mb-44 ">
-          <LinearGradient
-            colors={["#9A6B36", "#CCA373", "#9A6B36"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-            style={globalStyles.gradient}
-          >
-            <Button
-              color={"#03050C"}
-              title="Don't have an account? Sign Up"
-              onPress={() => navigation.navigate("SignUp")}
-            />
-          </LinearGradient>
-        </View> */}
-        <View className="mb-44 mt-4 ">
+        <View className="mb-44 mt-4">
           <ButtonComponent
             onPress={() => navigation.navigate("SignUp")}
             secondary={true}
@@ -105,7 +100,7 @@ const SignInScreen: FC = () => {
             title="Don't have an account? Sign Up"
           />
         </View>
-        <View className="mb-44 mt-4 ">
+        <View className="mb-44 mt-4">
           <ButtonComponent
             onPress={() => {
               dispatch(logAsGuest());
@@ -113,6 +108,14 @@ const SignInScreen: FC = () => {
             secondary={true}
             white={false}
             title="Continuer sans compte"
+          />
+        </View>
+        <View className="mb-44 mt-4">
+          <ButtonComponent
+            onPress={handleResetOnboarding}
+            secondary={true}
+            white={false}
+            title="False onboarding local storage"
           />
         </View>
       </View>
@@ -125,13 +128,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  input: {
-    width: "80%",
-    marginBottom: 10,
-    padding: 10,
-    borderColor: "gray",
-    borderWidth: 1,
   },
 });
 
