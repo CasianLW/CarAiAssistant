2a49a53514de569981c534cd0d8a54a9ad14f4af
CasianLW <ciorbacasian@gmail.com>
2024-06-11 08:17:10 +0200

    added some global styles

--- styles/global.styles.ts
+++ styles/global.styles.ts
@@ -18,12 +18,41 @@ export default StyleSheet.create({
     padding: 10,
     backgroundColor: "#EEECE9",
     borderRadius: 5,
+    fontFamily: "UrbanistMedium500",
+
     // backgroundColor: "#f5f5f5",
     // borderWidth: 1,
   },
+  headerTitle: {
+    fontFamily: "UrbanistSemiBold600",
+    position: "absolute",
+    top: 52,
+    width: "100%",
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
   title: {
+    fontFamily: "UrbanistSemiBold600",
     fontSize: 24,
-    fontWeight: "400",
+    fontWeight: "900",
+    color: "#000",
+    marginVertical: 10,
+  },
+  subtitle: {
+    fontFamily: "UrbanistMedium500",
+    fontSize: 20,
+    marginVertical: 10,
+    color: "#337AFF",
+  },
+  footerText: {
+    fontFamily: "UrbanistMedium500",
+    width: "100%",
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginVertical: 20,
   },
 
   gradient: {
