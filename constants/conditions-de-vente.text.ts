4ec38a982c49860e543cfa4728808fdcb28cb08a
CasianLW <ciorbacasian@gmail.com>
2024-06-11 10:05:25 +0200

    paragraphe conditions et termes

---
+++ constants/conditions-de-vente.text.ts
@@ -0,0 +1,32 @@
+const conditionsAppText = `
+1. Acceptation des Termes
+En téléchargeant ou en utilisant l'application CarApp, ces termes vous lieront. Si vous n'acceptez pas les termes, n'utilisez pas cette application. L'utilisation de l'application signifie que vous acceptez ces termes.
+
+2. Description du Service
+CarApp est une application mobile qui assiste les utilisateurs à trouver des véhicules d'occasion en fonction de leurs besoins spécifiques et de leurs préférences. L'application propose des recommandations personnalisées basées sur des algorithmes qui analysent les entrées des utilisateurs.
+
+3. Politique de Confidentialité
+Les informations personnelles soumises à travers l'application sont protégées conformément à notre politique de confidentialité. Pour plus d'informations, veuillez consulter la politique de confidentialité de l'application.
+
+4. Accès à l'Application
+L'accès à certaines parties de l'application peut nécessiter l'utilisateur à créer un compte. Vous êtes responsable de la sécurité de votre mot de passe et vous serez seul responsable de toutes les activités effectuées sous votre compte.
+
+5. Propriété Intellectuelle
+Tous les contenus inclus dans l'application, tels que les textes, graphiques, logos, images, ainsi que leur sélection et arrangement, sont la propriété exclusive de CarApp ou de ses fournisseurs de contenu et sont protégés par les lois sur les droits d'auteur.
+
+6. Utilisation Acceptable
+Vous vous engagez à ne pas utiliser l'application d'une manière qui pourrait endommager, désactiver, surcharger ou altérer le service ou interférer avec l'utilisation et la jouissance du service par toute autre partie.
+
+7. Modifications des Termes
+Nous nous réservons le droit de modifier les termes et conditions à tout moment. Votre utilisation continue de l'application après de telles modifications constituera votre accord à suivre et être lié par les termes modifiés.
+
+8. Limitation de Responsabilité
+En aucun cas, CarApp ne sera tenu responsable des dommages indirects, accidentels, spéciaux, consécutifs ou exemplaires découlant de l'utilisation ou de l'impossibilité d'utiliser les services ou l'application.
+
+9. Résiliation
+Nous pouvons résilier ou suspendre votre accès à l'application immédiatement, sans préavis ni responsabilité, pour quelque raison que ce soit, y compris sans limitation si vous violez les Termes et Conditions.
+
+10. Contactez-Nous
+Pour toute question ou préoccupation concernant ces termes, veuillez nous contacter à carapp@support.com.
+`;
+export default conditionsAppText;
