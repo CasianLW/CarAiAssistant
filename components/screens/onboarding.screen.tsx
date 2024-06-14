165cc25f1289675f1438995d54dbe81400716390
CasianLW <ciorbacasian@gmail.com>
2024-06-14 21:17:31 +0200

    onboard screen design / front component

--- components/screens/onboarding.screen.tsx
+++ components/screens/onboarding.screen.tsx
@@ -1,19 +1,26 @@
 import React, { FC, useState, useRef } from "react";
-import { View, Text, Image, Animated, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Animated,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  ScrollView,
+  Button,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthStackParamList } from "@/app/_layout";
 import ButtonComponent from "../general/button-component";
 import globalStyles from "@/styles/global.styles";
-
-type SignInNavigationProp = StackNavigationProp<AuthStackParamList, "SignIn">;
+import ArrowBackIcon from "@/assets/images/icons/arrow-back.icon";
+import conditionsAppText from "@/constants/conditions-de-vente.text";
+import { useOnboard } from "@/hooks/onboard.hook";
 
 const OnboardingScreen: FC = () => {
-  const navigation = useNavigation<SignInNavigationProp>();
-  const offsetX = useRef(new Animated.Value(0)).current; // Animation for car image
-  const [slide, setSlide] = useState(0); // Current slide index
-
   const titles = [
     "Bienvenue sur CarApp",
     "La puissance de l’intelligence artificielle entre vos mains",
@@ -25,50 +32,41 @@ const OnboardingScreen: FC = () => {
     "Spécialement conçue pour le domaine de l’achat auto d’occasion",
     "Liée aux sites d’annonces d’occasion en ligne pour une recherche facilitée",
   ];
-
-  const moveDistance = 380;
-  const handlePrevious = () => {
-    if (slide > 0) {
-      Animated.timing(offsetX, {
-        toValue: -(slide - 1) * moveDistance, // Move image to the right
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-
-      setSlide(slide - 1); // Move to the previous slide
-    }
-  };
-
-  const handleNext = async () => {
-    if (slide === 2) {
-      await AsyncStorage.setItem("hasOnboarded", "true");
-      navigation.replace("SignIn");
-    } else {
-      Animated.timing(offsetX, {
-        toValue: -(slide + 1) * moveDistance, // Move image to the left
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-
-      setSlide(slide + 1); // Move to the next slide
-    }
-  };
+  const {
+    slide,
+    navigation,
+    modalVisible,
+    setModalVisible,
+    offsetX,
+    offsetX2,
+    handlePrevious,
+    handleNext,
+    dotWidths,
+  } = useOnboard(titles);
 
   return (
     <View style={styles.container}>
       <Text style={globalStyles.headerTitle}>CarApp</Text>
+      {slide > 0 && (
+        <TouchableOpacity
+          onPress={handlePrevious}
+          style={globalStyles.arrowBack}
+        >
+          <ArrowBackIcon />
+        </TouchableOpacity>
+      )}
       <Animated.Image
         source={require("@/assets/images/onboarding/decapotable.png")}
         style={[styles.image, { transform: [{ translateX: offsetX }] }]}
       />
       <Animated.Image
         source={require("@/assets/images/onboarding/audi-rs.png")}
-        style={[styles.image2, { transform: [{ translateX: offsetX }] }]}
+        style={[styles.image2, { transform: [{ translateX: offsetX2 }] }]}
       />
       <View style={styles.contentContainer}>
         <Text style={globalStyles.title}>{titles[slide]}</Text>
         <Text style={globalStyles.subtitle}>{subtitles[slide]}</Text>
-        <View style={styles.pagination}>
+        {/* <View style={styles.pagination}>
           {titles.map((_, index) => (
             <View
               key={index}
@@ -78,22 +76,58 @@ const OnboardingScreen: FC = () => {
               ]}
             />
           ))}
+        </View> */}
+        <View style={styles.pagination}>
+          {titles.map((_, index) => (
+            <Animated.View
+              key={index}
+              style={[
+                styles.paginationDot,
+                { width: dotWidths[index] }, // Use the animated width here
+                index === slide && styles.paginationDotActive,
+              ]}
+            />
+          ))}
         </View>
         <View style={styles.buttonsContainer}>
+          {slide < 2 && (
+            <ButtonComponent
+              style={styles.buttonFlex}
+              secondary={true}
+              title="Passer"
+              onPress={() => navigation.replace("SignIn")}
+            />
+          )}
           <ButtonComponent
-            secondary={true}
-            title="Passer"
-            onPress={() => navigation.replace("SignIn")}
-          />
-          <ButtonComponent
-            title={slide === 2 ? "Terminer" : "Suivant"}
+            style={styles.buttonFlex}
+            title={slide === 2 ? "Commencer l’aventure !" : "Suivant"}
             onPress={handleNext}
           />
         </View>
         <Text style={globalStyles.footerText}>
-          En continuant, vous acceptez nos termes et conditions
+          En continuant, vous acceptez nos{" "}
+          <Text style={styles.underline} onPress={() => setModalVisible(true)}>
+            termes et conditions
+          </Text>
         </Text>
       </View>
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Termes et Conditions</Text>
+            <ScrollView style={styles.modalBody}>
+              <Text>{conditionsAppText}</Text>
+              {/* Add more content here if needed for the conditions (title/text structure) */}
+            </ScrollView>
+            <Button title="Fermer" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -107,21 +141,26 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "flex-start",
   },
-
+  backButtonIcon: {
+    width: 24,
+    height: 24,
+    color: "#FFFFFF",
+  },
   image: {
     width: "120%",
     height: "50%",
     resizeMode: "contain",
-    marginLeft: 100,
-    marginTop: 40,
-    // display: "none",
+    position: "absolute",
+    top: 40,
+    left: 100,
   },
   image2: {
     width: "100%",
     height: "50%",
     resizeMode: "contain",
-    marginLeft: -380,
-    marginTop: 40,
+    position: "absolute",
+    // top: 40,
+    left: 100,
   },
   contentContainer: {
     width: "100%",
@@ -134,7 +173,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     position: "absolute",
   },
-
   pagination: {
     flexDirection: "row",
     marginVertical: 20,
@@ -151,8 +189,39 @@ const styles = StyleSheet.create({
     width: 28,
   },
   buttonsContainer: {
+    display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
+  },
+  buttonFlex: {
+    flex: 1,
+  },
+  underline: {
+    textDecorationLine: "underline",
+    color: "#0066FF",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    padding: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontFamily: "UrbanistSemiBold600",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalBody: {
+    fontFamily: "UrbanistMedium500",
+    maxHeight: 360,
+    marginBottom: 20,
   },
 });
