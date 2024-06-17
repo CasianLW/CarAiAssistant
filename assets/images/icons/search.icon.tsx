28cee9e2f58d90caa7de14d1747a35dd249bc83a
CasianLW <ciorbacasian@gmail.com>
2024-06-17 16:38:20 +0200

    removed stroke color

--- assets/images/icons/search.icon.tsx
+++ assets/images/icons/search.icon.tsx
@@ -6,12 +6,12 @@ const SearchIcon: React.FC<SvgProps> = (props) => (
   <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
     <Path
       d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18Z"
-      stroke="white"
+      // stroke="white"
       stroke-width="2"
     />
     <Path
       d="M20 20L17 17"
-      stroke="white"
+      // stroke="white"
       stroke-width="2"
       stroke-linecap="round"
     />
