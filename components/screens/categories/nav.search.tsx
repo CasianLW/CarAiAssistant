dc331c09ba5458c286e54ae8a76f36c30aee4880
CasianLW <ciorbacasian@gmail.com>
2024-06-20 23:39:59 +0200

    bettter nav logic

--- components/screens/categories/nav.search.tsx
+++ components/screens/categories/nav.search.tsx
@@ -4,17 +4,19 @@ import React, { FC } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 interface NavSearchProps {
-  goBack: () => void;
+  goBack?: () => void;
 }
 const NavSearch: FC<NavSearchProps> = ({ goBack }) => {
   return (
-    <View className="w-full h-24 pt-12">
-      <TouchableOpacity
-        className="w-10 h-10 absolute top-14 left-6"
-        onPress={goBack}
-      >
-        <GoBackIcon fill={"#fff"} />
-      </TouchableOpacity>
+    <View className="w-full h-24 pt-12 bg-app-blue-100">
+      {goBack && (
+        <TouchableOpacity
+          className="w-10 h-10 absolute top-14 left-6"
+          onPress={goBack}
+        >
+          <GoBackIcon fill={"#fff"} />
+        </TouchableOpacity>
+      )}
 
       <Text
         className="text-app-white-100 text-xl m-auto"
