b5d1f8992ef95bc61caae6644e6bfdc674f073ca
CasianLW <ciorbacasian@gmail.com>
2024-06-20 23:37:41 +0200

    added search results screen

--- components/tabs/sharedScreens.tsx
+++ components/tabs/sharedScreens.tsx
@@ -5,11 +5,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SearchUnknownScreen from "@/components/screens/categories/unknown.search";
 import SearchCarScreen from "@/components/screens/categories/car.search";
 import SearchCategoryScreen from "@/components/screens/categories/category.search";
+import SearchResultsScreen from "../screens/results/results.screen";
 
 export type SharedTabParamList = {
   SearchUnknown: undefined;
   SearchCar: undefined;
   SearchCategory: undefined;
+  SearchResults: undefined;
 };
 
 const { Screen } = createBottomTabNavigator<SharedTabParamList>();
@@ -31,6 +33,12 @@ export const sharedScreens = [
     key="SearchCategory"
     name="SearchCategory"
     component={SearchCategoryScreen}
+    options={{ tabBarButton: () => null, headerShown: false }}
+  />,
+  <Screen
+    key="SearchResults"
+    name="SearchResults"
+    component={SearchResultsScreen}
     options={{ tabBarButton: () => null, headerShown: false }}
   />,
 ];
