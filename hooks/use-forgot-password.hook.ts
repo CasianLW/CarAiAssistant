bbf4a88a4df1a9d1233fc0653023e279e2b65858
CasianLW <ciorbacasian@gmail.com>
2024-06-28 14:59:41 +0200

    updated hook with /validators, errors handling, modal states and loading// removed cognito and used our api instead

--- hooks/use-forgot-password.hook.ts
+++ hooks/use-forgot-password.hook.ts
@@ -1,48 +1,112 @@
 import { useState } from "react";
-import { Auth } from "aws-amplify";
+import { apiForgotPassword, apiResetPassword } from "@/utils/api";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthStackParamList } from "@/app/_layout";
 
-type SignInNavigationProp = StackNavigationProp<AuthStackParamList, "SignIn">;
+interface ForgotPasswordError {
+  email?: string;
+  newPassword?: string;
+  message?: string;
+}
 
 const useForgotPassword = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
   const [newPassword, setNewPassword] = useState("");
-  const navigation = useNavigation<SignInNavigationProp>();
+  const [errors, setErrors] = useState<ForgotPasswordError>({});
   const [isResetModalVisible, setIsResetModalVisible] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const navigation =
+    useNavigation<StackNavigationProp<AuthStackParamList, "SignIn">>();
+
+  const validateEmail = () => {
+    let isValid = true;
+    const newErrors: ForgotPasswordError = {};
+
+    if (!email) {
+      newErrors.email = "L'email est obligatoire";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "L'email n'est pas valide";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const validateNewPassword = () => {
+    if (newPassword.length < 6) {
+      setErrors((prev) => ({
+        ...prev,
+        newPassword: "Le mot de passe doit contenir au moins 6 caractères",
+      }));
+      return false;
+    }
+    return true;
+  };
 
   const handleForgotPassword = async () => {
+    if (!validateEmail()) return;
+    setIsProcessing(true);
     try {
-      await Auth.forgotPassword(username);
+      await apiForgotPassword({ email });
       setIsResetModalVisible(true);
+      setErrors({}); // Clear errors on success
     } catch (error) {
-      console.error("Forgot password error", error);
+      // console.error("Forgot password error", error);
+      setErrors({ message: "Email non trouvé, veuillez réessayer." });
+    } finally {
+      setIsProcessing(false);
     }
   };
 
   const handlePasswordResetConfirm = async () => {
+    if (!validateNewPassword()) return;
+    setIsProcessing(true);
     try {
-      await Auth.forgotPasswordSubmit(username, code, newPassword);
+      await apiResetPassword({
+        email,
+        code,
+        newPassword,
+        confirmPassword: newPassword,
+      });
       setIsResetModalVisible(false);
-      navigation.navigate("SignIn");
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+        navigation.navigate("SignIn");
+      }, 3000); // Wait for 3 seconds before navigating
+      setErrors({}); // Clear errors on success
     } catch (error) {
-      console.error("Password reset confirmation error", error);
+      // console.error("Password reset confirmation error", error);
+      setErrors({
+        message:
+          "Erreur lors de la réinitialisation. Assurez-vous que le code et le nouveau mot de passe sont corrects.",
+      });
+    } finally {
+      setIsProcessing(false);
     }
   };
 
   return {
-    username,
-    setUsername,
+    email,
+    setEmail,
     code,
     setCode,
     newPassword,
     setNewPassword,
+    errors,
+    setErrors,
     isResetModalVisible,
     setIsResetModalVisible,
     handleForgotPassword,
     handlePasswordResetConfirm,
+    isProcessing,
+    showSuccessMessage,
   };
 };
 
