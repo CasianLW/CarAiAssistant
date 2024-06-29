6e778ec1e3403cfc05df4d7fb6a7d9d7351b5384
CasianLW <ciorbacasian@gmail.com>
2024-06-29 03:52:29 +0200

    created interfaces for ai api data

---
+++ interfaces/api-datas.ts
@@ -0,0 +1,38 @@
+import { FilterData } from "@/components/screens/categories/steps/filter.step";
+import { MobiledeCarCardProps } from "./mobilede-car";
+
+export interface AiPayloadUnknown {
+  userId: string | null;
+  userLogged: boolean;
+  prompt: string;
+  // filters: FilterData;
+}
+
+// export interface VehicleAiResponse {
+//   make: string;
+//   makeId: number;
+//   model: string;
+//   modelId: number;
+//   year: number;
+//   price: number;
+//   consumption: number;
+//   fuel_cost: number;
+//   annual_maintenance: number;
+//   registration_cost: number;
+//   estimated_insurance: number;
+//   max_km: number;
+//   description: string;
+// }
+
+export interface VehicleAiApiResponse {
+  statusCode: number;
+  message: string;
+  data: {
+    respectedFilters: boolean;
+    vehicles: MobiledeCarCardProps[];
+  };
+}
+
+export interface VehicleAiData extends VehicleAiApiResponse {
+  filters: FilterData;
+}
