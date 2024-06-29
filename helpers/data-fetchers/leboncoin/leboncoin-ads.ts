e80f34a8940a15210bdd74096ec9fbdc6231cd82
CasianLW <ciorbacasian@gmail.com>
2024-06-29 18:41:52 +0200

    helper function to models containing spaces or '-'; also upper case adaptations for url & max mins by default

--- helpers/data-fetchers/leboncoin/leboncoin-ads.ts
+++ helpers/data-fetchers/leboncoin/leboncoin-ads.ts
@@ -12,6 +12,9 @@ const userAgents = [
   "Mozilla/5.0 (X11; CrOS x86_64 15633.69.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.212 Safari/537.36",
   "Mozilla/5.0 (X11; CrOS x86_64 15633.69.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.212 Safari/537.36",
 ];
+function replaceSeparators(inputString: string) {
+  return inputString.replace(/[- _]/g, "%20");
+}
 
 export function useFetchLeboncoinCars({
   make,
@@ -30,20 +33,23 @@ export function useFetchLeboncoinCars({
   const [error, setError] = useState<string | null>(null);
 
   const fetchCars = useCallback(async () => {
-    if (!make || !model || !maxYear) {
+    if (!make || !model || !minYear) {
       console.error("Make, model, and year are required parameters (lbc).");
+      // console.error("Data:", { make, model, maxYear });
       return;
     }
 
     const baseUrl = "https://www.leboncoin.fr/recherche";
     // let url = `${baseUrl}?category=2&u_car_brand=${make}&u_car_model=${make}_${model}&regdate=${year}-max`;
-    let url = `${baseUrl}?category=2&u_car_brand=${make}&u_car_model=${make}_${model}&price=${
-      minPrice ? minPrice : 200
-    }-${maxPrice ? maxPrice : 5000000}&regdate=${minYear ? minYear : 1950}-${
-      maxYear ? maxYear : 2050
-    }&mileage=${minKm ? minKm : 0}-${
-      maxKm ? maxKm : 1000000
-    }&sort=price&order=asc`;
+    let url = `${baseUrl}?category=2&u_car_brand=${make.toUpperCase()}&u_car_model=${make.toUpperCase()}_${replaceSeparators(
+      model
+    )}&price=${minPrice ? minPrice : 200}-${
+      maxPrice ? maxPrice : 5000000
+    }&regdate=${minYear ? minYear : 1950}-${maxYear ? maxYear : 2050}&mileage=${
+      minKm ? minKm : 0
+    }-${maxKm ? maxKm : 1000000}&sort=price&order=asc`;
+
+    console.log("URL lbc:", url);
 
     // const defaultFilters = {
     //   srt: "price",
@@ -54,6 +60,10 @@ export function useFetchLeboncoinCars({
     // const filterParams = Object.entries(defaultFilters)
     //   .map(([key, value]) => `${key}=${value}`)
     //   .join("&");
+
+    // https://www.leboncoin.fr/recherche?category=2&u_car_brand=PEUGEOT&u_car_model=PEUGEOT_208&price=15000-37800&regdate=2020-2050&mileage=0-200000&sort=price&order=asc
+
+    // https://www.leboncoin.fr/recherche?category=2&u_car_brand=PEUGEOT&u_car_model=PEUGEOT_208&price=15000-37800&regdate=2020-2050&mileage=0-200000&sort=price&order=asc
 
     // url += `&${filterParams}`;
 
@@ -67,7 +77,7 @@ export function useFetchLeboncoinCars({
     setLoading(true);
 
     try {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
+      await new Promise((resolve) => setTimeout(resolve, 500));
       const response = await axios.get(url, { headers });
 
       const htmlContent: string = response.data.toString();
