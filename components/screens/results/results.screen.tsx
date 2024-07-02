7017b7ddeed963682fa937abda1fe7838740d888
CasianLW <ciorbacasian@gmail.com>
2024-07-02 19:27:40 +0200

    implemented 'voir tout X' urls btns & voir rapport design

--- components/screens/results/results.screen.tsx
+++ components/screens/results/results.screen.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import {
   View,
   Text,
@@ -11,8 +11,27 @@ import globalStyles from "@/styles/global.styles";
 import AdsResultsComponent from "./components/ads-results.component";
 import AiResultCardComponent from "./components/ai-result-card.component";
 import useSearchResults, { AdsPlatform } from "./results.hook";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { SharedTabParamList } from "@/components/tabs/sharedScreens";
+import ButtonComponent from "@/components/general/button-component";
+import { MobiledeCarCardProps } from "@/interfaces/mobilede-car";
+import { openURI } from "@/helpers/open-uri";
+import { apiRapportData } from "@/utils/api";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/main-store";
+import { RapportPayloadData } from "@/interfaces/api-datas";
+
+type SearchResultsRouteProp = RouteProp<SharedTabParamList, "SearchResults">;
 
 const SearchResultsScreen: FC = () => {
+  const { userData, isGuest } = useSelector((state: RootState) => state.auth);
+  const route = useRoute<SearchResultsRouteProp>();
+  const { results: apiResponse } = route.params ?? { results: null };
+  const [chosenCarToFetch, setChosenCarToFetch] =
+    useState<MobiledeCarCardProps | null>(null);
+  const [reportVisible, setReportVisible] = useState(false);
+  const [reportData, setReportData] = useState(null);
+
   const {
     loading,
     error,
@@ -22,18 +41,46 @@ const SearchResultsScreen: FC = () => {
     setOpenResults,
     results,
     allResults,
+    setAllResults,
+    setFilteredResults,
     totalPages,
     nextPage,
     previousPage,
     currentPage,
     paginatedResults,
-  } = useSearchResults();
+    seeAllUrlMobilede,
+    seeAllUrlLeboncoin,
+    seeAllUrlAutoscout,
+    carsAutoscout,
+    carsLeboncoin,
+    carsMobilede,
+  } = useSearchResults(
+    apiResponse?.data.vehicles || [],
+    chosenCarToFetch,
+    apiResponse?.filters || {
+      minPrice: "",
+      maxPrice: "",
+      minYear: "",
+      maxYear: "",
+      minKm: "",
+      maxKm: "",
+      minHp: "",
+      maxHp: "",
+    }
+  );
 
   const scrollViewRef = useRef<ScrollView>(null);
 
   const [activeFilter, setActiveFilter] = React.useState<AdsPlatform>(
     AdsPlatform.allPlatforms
   );
+
+  const handleGoBack = () => {
+    setReportData(null);
+    setOpenResults(false);
+    setAllResults([]);
+    setFilteredResults([]);
+  };
 
   const handleSetPlatform = (platform: AdsPlatform) => {
     setActiveFilter(platform);
@@ -50,21 +97,50 @@ const SearchResultsScreen: FC = () => {
     previousPage();
     scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: true });
   };
+  const fetchReportData = async () => {
+    if (!chosenCarToFetch || isGuest || !userData) return;
+    const rapportData: RapportPayloadData = {
+      userId: userData.userId,
+      userLogged: !isGuest,
+      vehicleData: chosenCarToFetch,
+    };
+    try {
+      console.log("Rapport data:", rapportData);
+      const response = await apiRapportData(rapportData);
 
+      console.log("Rapport data:", response.data);
+      setReportData(response.data);
+      setReportVisible(true);
+    } catch (error) {
+      console.error("Failed to fetch report:", error);
+      // Handle errors (e.g., show a message)
+    }
+  };
+  const handleReport = () => {
+    if (!reportData) {
+      fetchReportData();
+    } else if (!reportVisible) {
+      setReportVisible(false);
+    } else if (reportVisible) {
+      setReportVisible(true);
+    }
+  };
   return (
     <View className="bg-app-blue-100" style={styles.container}>
-      <NavSearch
-        goBack={openResults ? () => setOpenResults(false) : undefined}
-      />
+      <NavSearch goBack={openResults ? () => handleGoBack() : undefined} />
+
       {openResults && filteredResults.length > 0 && (
-        <View className="bg-app-blue-100  px-4 py-2 absolute top-32 right-0 z-20 rounded-l-xl ">
+        <TouchableOpacity
+          className="bg-app-blue-100  px-4 py-2 absolute top-32 right-0 z-20 rounded-l-xl "
+          onPress={handleReport}
+        >
           <Text
             style={{ fontFamily: "UrbanistSemiBold600" }}
             className="text-app-white-200"
           >
             Voir rapport
           </Text>
-        </View>
+        </TouchableOpacity>
       )}
       <ScrollView
         className="h-full"
@@ -95,15 +171,17 @@ const SearchResultsScreen: FC = () => {
           <View className="mb-40">
             {results.map((result, index) => (
               <AiResultCardComponent
-                onButton={() => setOpenResults(true)}
+                onFetch={setChosenCarToFetch}
+                // onFetch={() => setOpenResults(true)}
                 car={result}
                 key={`${result.makeId}-${result.modelId}-${index}`}
               />
             ))}
           </View>
         )}
+
         {openResults && filteredResults.length > 25 && (
-          <View className="flex flex-row justify-between  items-center mb-40">
+          <View className="flex flex-row justify-between  items-center mb-4">
             <TouchableOpacity
               onPress={handlePreviousPage}
               disabled={currentPage === 1}
@@ -131,6 +209,29 @@ const SearchResultsScreen: FC = () => {
             </TouchableOpacity>
           </View>
         )}
+        <View className="mb-40 ">
+          {openResults && seeAllUrlMobilede && carsMobilede.length > 0 && (
+            <ButtonComponent
+              title="Voir tout mobile.de"
+              onPress={() => openURI(seeAllUrlMobilede)}
+            />
+          )}
+          {openResults && seeAllUrlLeboncoin && carsLeboncoin.length > 0 && (
+            <View>
+              <ButtonComponent
+                title="Voir tout leboncoin"
+                onPress={() => openURI(seeAllUrlLeboncoin)}
+              />
+              {/* <Text>Leboncoin: {seeAllUrlLeboncoin}</Text> */}
+            </View>
+          )}
+          {openResults && seeAllUrlAutoscout && carsAutoscout.length > 0 && (
+            <ButtonComponent
+              title="Voir tout autoscout"
+              onPress={() => openURI(seeAllUrlAutoscout)}
+            />
+          )}
+        </View>
       </ScrollView>
 
       {openResults && (
