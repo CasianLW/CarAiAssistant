ac4f5f9ec5303192b546bd02853908304d656ecc
CasianLW <ciorbacasian@gmail.com>
2024-07-06 23:20:09 +0200

    added logic to link category search with api on same api endpont then on response be redirected to results scren

--- components/screens/categories/category.search.tsx
+++ components/screens/categories/category.search.tsx
@@ -1,5 +1,13 @@
-import React, { useState, FC, useEffect, useCallback } from "react";
-import { View, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React, { useState, FC, useCallback } from "react";
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ActivityIndicator,
+  Alert,
+  Modal,
+} from "react-native";
 import globalStyles from "@/styles/global.styles";
 import { useNavigation } from "@react-navigation/native";
 import NavSearch from "./nav.search";
@@ -10,11 +18,58 @@ import CategoryStep from "./steps/category.step";
 import ButtonComponent from "@/components/general/button-component";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { SharedTabParamList } from "@/components/tabs/sharedScreens";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/main-store";
+import { apiProcessVehicleData } from "@/utils/api";
+import { VehicleAiApiResponse } from "@/interfaces/api-datas";
+import { MobiledeCarCardProps } from "@/interfaces/mobilede-car";
+
+function validateApiResponse(data: any): VehicleAiApiResponse | null {
+  if (
+    typeof data === "object" &&
+    typeof data.statusCode === "number" &&
+    typeof data.message === "string" &&
+    typeof data.data === "object" &&
+    typeof data.data.respectedFilters === "boolean" &&
+    Array.isArray(data.data.vehicles) &&
+    data.data.vehicles.every(isValidVehicle)
+  ) {
+    return data as VehicleAiApiResponse;
+  } else {
+    return (data = null);
+  }
+}
+
+function isValidVehicle(object: any): object is MobiledeCarCardProps {
+  return (
+    typeof object === "object" &&
+    typeof object.make === "string" &&
+    typeof object.makeId === "number" &&
+    typeof object.model === "string" &&
+    typeof object.modelId === "number" &&
+    typeof object.year === "number" &&
+    typeof object.price === "number" &&
+    typeof object.consumption === "number" &&
+    typeof object.fuel_cost === "number" &&
+    typeof object.annual_maintenance === "number" &&
+    typeof object.registration_cost === "number" &&
+    typeof object.estimated_insurance === "number" &&
+    typeof object.max_km === "number" &&
+    typeof object.description === "string"
+  );
+}
 
 const SearchCategoryScreen: FC = () => {
+  const { userData, isGuest } = useSelector((state: RootState) => state.auth);
+  const navigation = useNavigation<StackNavigationProp<SharedTabParamList>>();
+
   const [currentStep, setCurrentStep] = useState<number>(0);
   const [categoryData, setCategoryData] = useState<string>("");
   const [chatData, setChatData] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [isErrorModalVisible, setIsErrorModalVisible] = useState(false);
+
   const [filterData, setFilterData] = useState<FilterData>({
     minPrice: "",
     maxPrice: "",
@@ -26,16 +81,65 @@ const SearchCategoryScreen: FC = () => {
     minYear: "",
   });
 
-  const navigation = useNavigation<StackNavigationProp<SharedTabParamList>>();
+  const steps = ["Category", "Chat", "Filter"];
 
-  const steps = ["Category", "Filter", "Chat"];
+  const processFinalData = async () => {
+    setIsLoading(true);
+    let filterDescriptions = [];
+    for (const [key, value] of Object.entries(filterData)) {
+      if (value) {
+        filterDescriptions.push(`${key}: ${value}`);
+      }
+    }
+    const filterString =
+      filterDescriptions.length > 0
+        ? ` Filters: ${filterDescriptions.join(", ")}`
+        : "";
+    const completePrompt = `Search me strictly ${categoryData} cars. ${chatData}${filterString}`;
+
+    try {
+      const response = await apiProcessVehicleData({
+        userId: userData?.userId || null,
+        userLogged: !isGuest,
+        prompt: completePrompt,
+      });
+
+      console.log("Response received:", response);
+
+      const validResults = validateApiResponse(response.data);
+      if (validResults) {
+        const completeData = { ...validResults, filters: filterData };
+        navigation.navigate("SearchResults", { results: completeData });
+        setError("");
+        setFilterData({
+          minPrice: "",
+          maxPrice: "",
+          maxHp: "",
+          minHp: "",
+          maxKm: "",
+          minKm: "",
+          maxYear: "",
+          minYear: "",
+        });
+        setChatData("");
+      } else {
+        console.error("Invalid response structure:", response.data);
+        setError("Le format de la réponse n'est pas celui attendu.");
+        setIsErrorModalVisible(true);
+      }
+    } catch (error) {
+      console.error("Error processing data:", error);
+      setError("Une erreur est survenue, réessayez.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const nextStep = () => {
     if (currentStep < steps.length - 1) {
       setCurrentStep(currentStep + 1);
     } else {
-      console.log("Final data:", { categoryData, filterData, chatData });
-      navigation.navigate("SearchResults");
+      processFinalData();
     }
   };
 
@@ -52,14 +156,36 @@ const SearchCategoryScreen: FC = () => {
 
   const isNextButtonDisabled = useCallback(() => {
     if (currentStep === 0) return !categoryData;
-    // if (currentStep === 1) return !filterData.minPrice || !filterData.maxPrice;
-    if (currentStep === 2) return !chatData;
+    if (currentStep === 1) return !chatData;
     return false;
   }, [currentStep, categoryData, chatData]);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View className="bg-app-blue-100">
+        {isLoading && (
+          <View style={globalStyles.loadingOverlay}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        )}
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isErrorModalVisible}
+          onRequestClose={() => setIsErrorModalVisible(false)}
+        >
+          <View style={globalStyles.modalCenteredView}>
+            <View style={globalStyles.modalView}>
+              <Text className="text-red-500">{error}</Text>
+              <ButtonComponent
+                title="Fermer"
+                onPress={() => setIsErrorModalVisible(false)}
+              />
+            </View>
+          </View>
+        </Modal>
+
         <NavSearch goBack={goBack} />
         <View style={globalStyles.searchContainer}>
           <Text style={globalStyles.title}>Recherche par catégorie:</Text>
@@ -67,12 +193,12 @@ const SearchCategoryScreen: FC = () => {
           {currentStep === 0 && (
             <CategoryStep setCategoryData={setCategoryData} />
           )}
-          {currentStep === 1 && <FilterStep setFilterData={setFilterData} />}
-          {currentStep === 2 && <ChatStep setChatData={setChatData} />}
+          {currentStep === 1 && <ChatStep setChatData={setChatData} />}
+          {currentStep === 2 && <FilterStep setFilterData={setFilterData} />}
           <ButtonComponent
+            disabledAction={isNextButtonDisabled()}
             title={buttonName}
             onPress={nextStep}
-            disabledAction={isNextButtonDisabled()}
           />
         </View>
       </View>
